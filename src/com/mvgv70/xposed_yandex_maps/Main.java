663a061cab85e1304bc87af05715a7fa14fed47c
mvg-v70 <gusev-mv@yandex.ru>
2015-10-01 19:59:38 +0300

    version 1.1

--- src/com/mvgv70/xposed_yandex_maps/Main.java
+++ src/com/mvgv70/xposed_yandex_maps/Main.java
@@ -4,64 +4,50 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
 
 public class Main implements IXposedHookLoadPackage {
 	
   private static ImageButton findmeButton = null;
   private final static String TAG = "xposed-yandex-maps";
+  
+  private Runnable pressButton = new Runnable()
+  {
+    public void run() 
+    {
+      findmeButton.performClick();
+      Log.d(TAG,"findmeButton pressed");
+	}
+  };
+    
+  private LocationListener locationListener = new LocationListener() 
+  {
+    public void onLocationChanged(Location location)
+    {
+      Log.d(TAG,"GPS postion detected");
+      // нажмем на FindMeButton через 1 сек и через 2 сек
+      findmeButton.postDelayed(pressButton, 1000);
+      findmeButton.postDelayed(pressButton, 2000);
+    }
+      
+    public void onProviderDisabled(String provider) {}
+      
+    public void onProviderEnabled(String provider) {}
+      
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+  };
 	
-  @SuppressLint("HandlerLeak")
   @Override
   public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-    
-    final Handler handler = new Handler() {
-    	
-      @Override
-      public void handleMessage(Message msg) {
-        if (findmeButton == null) return;
-        // может и не нужно проверять видимость
-  	    if (findmeButton.getVisibility() == View.VISIBLE)
-    	{
-          findmeButton.performClick();
-          Log.d(TAG,"findmeButton pressed");
-          if (msg.what > 0)
-          {
-            // нажмем на FindMeButton через 1 сек
-        	int what = msg.what-1;
-            sendEmptyMessageDelayed(what, 1000);
-          }
-    	}
-      }
-    };
-    
-    final LocationListener locationListener = new LocationListener() {
-    	
-      public void onLocationChanged(Location location)
-      {
-        Log.d(TAG,"GPS postion detected");
-        // нажмем на FindMeButton через 1 сек
-        handler.sendEmptyMessageDelayed(1, 1000);
-      }
-      
-      public void onProviderDisabled(String provider) {}
-      
-      public void onProviderEnabled(String provider) {}
-      
-      public void onStatusChanged(String provider, int status, Bundle extras) {}
-    };
     
     // FindMeButton(Context, AttributeSet)
     XC_MethodHook createFindMeButton = new XC_MethodHook() {
@@ -80,6 +66,13 @@ public class Main implements IXposedHookLoadPackage {
       protected void afterHookedMethod(MethodHookParam param) throws Throwable {
         Log.d(TAG,"Activity.create");
         Activity activity = (Activity)param.thisObject;
+        // показать версию модуля
+        try 
+        {
+     	  Context context = activity.createPackageContext(getClass().getPackage().getName(), Context.CONTEXT_IGNORE_SECURITY);
+     	  String version = context.getString(R.string.app_version_name);
+          Log.d(TAG,"version="+version);
+     	} catch (NameNotFoundException e) {}
         LocationManager locationManager = (LocationManager)activity.getSystemService(Context.LOCATION_SERVICE);
         if (locationManager != null) 
           // одноразовое определение координат
@@ -95,6 +88,5 @@ public class Main implements IXposedHookLoadPackage {
     XposedHelpers.findAndHookMethod("ru.yandex.yandexmaps.MapActivity", lpparam.classLoader, "onCreate", Bundle.class,  createActivity);
     Log.d(TAG,"ru.yandex.yandexmaps OK");
   }
-  
 
 }
